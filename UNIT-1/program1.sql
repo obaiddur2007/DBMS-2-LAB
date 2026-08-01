39491dc62da53a9edec0a263ac04d33b490a40c7
OBAIDDUR RAHMAN AJAJI <obaiddur2007@gmail.com>
2026-08-01 15:35:37 +0530

    Add files via upload

---
+++ UNIT-1/program1.sql
@@ -0,0 +1,21 @@
+--program calculate simple interest using pl/sql
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+	principal number:=10000;
+	rate      number:=8;
+	years	  number:=3;
+	interest  number;
+
+BEGIN
+	interest:=(principal*rate*years)/100;
+	dbms_output.put_line('principal amount:'||principal);
+	dbms_output.put_line('rate of interest:'||rate||'%');
+	dbms_output.put_line('time(years):'||years);
+	dbms_output.put_line('simple interest:'||interest);
+
+END;
+/
+	
+	
---
+++ UNIT-1/program2.sql
@@ -0,0 +1,21 @@
+-- Program: Calculate Square and Cube of a Given Number
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+	 num number := 5;
+         square number;
+         cube number;
+
+BEGIN
+	 -- Calculate Square and Cube
+    	 square := num * num;
+   	 	 cube := num * num * num;
+
+    	 -- Display the Result
+   	 DBMS_OUTPUT.PUT_LINE('number = ' || num);
+   	 DBMS_OUTPUT.PUT_LINE('square = ' || square);
+   	 DBMS_OUTPUT.PUT_LINE('cube = ' || cube);
+
+END;
+/
